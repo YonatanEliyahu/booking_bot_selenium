38b2ee4ac1ff25cf52eb0f714e834fae2e129797
YonatanEliyahu <106242827+YonatanEliyahu@users.noreply.github.com>
2023-12-27 12:03:37 +0200

    minor changes

--- booking/constants.py
+++ booking/constants.py
@@ -1,14 +1,14 @@
 DRIVER_PATH = "./chromedriver.exe"
 BASE_URL = "https://www.booking.com/"
 TIME_OUT = 7.5
-MAXMOUNTS = 12
+MAX_MONTHS = 12
 ADULTS_DEFAULT = 2
 MIN_NUM_ADULTS = 1
 MAX_NUM_ADULTS = 30
 MIN_NUM_ROOMS = 1
 MAX_NUM_ROOMS = 30
 MAX_NUM_CHILDREN = 10
-CHILDREN_DEFULT = 0
+CHILDREN_DEFAULT = 0
 
 CHILD_MIN_AGE = 0
 CHILD_MAX_AGE = 18
@@ -28,3 +28,8 @@ languages = {'IL': 'עברית',
              }
 
 flexible_dates_options = {0: 1, 1: 2, 2: 3, 3: 4, 7: 5}  # map flexibility to its element position in page
+
+flexible_duration_options = {'weekend', 'week', 'month', 'other'}
+
+MONTHS = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct',
+          11: 'Nov', 12: 'Dec'}

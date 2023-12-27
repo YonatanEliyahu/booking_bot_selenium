408628e8815aa72deab0eac7481b591ca10a479f
YonatanEliyahu <106242827+YonatanEliyahu@users.noreply.github.com>
2023-12-27 12:04:51 +0200

    script edited

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ def main():
         bot.change_lang(lang='US')
         bot.change_currency(curr="USD")
         bot.choose_destination("Tel Aviv")
-        bot.choose_dates(checkin="2023-12-26",
-                         checkout="2023-12-30")
-        bot.flexiable_dates(1)
+        bot.choose_vacation_dates(checkin="2023-12-30",
+                                 checkout="2024-01-05",
+                                 flex=1)
+        # bot.flexible_vacation("other-10-Sunday", [1, 5, 11])
         bot.select_num_adults(3)
-        bot.select_num_children([1,2,3,4,5,10])
+        bot.select_num_children([1, 2, 3, 4, 5, 10])
         bot.select_num_rooms(2)
         bot.submit_search()
         print("exiting ...")

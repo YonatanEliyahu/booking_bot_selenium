a81667779373821a312f06e0cba3d1f76f27f01d
YonatanEliyahu <106242827+YonatanEliyahu@users.noreply.github.com>
2023-12-27 12:02:48 +0200

    new functionalities added

--- booking/booking.py
+++ booking/booking.py
@@ -1,6 +1,6 @@
 import time
+import os
 from typing import List
-
 import selenium
 from selenium import webdriver
 from selenium.common import TimeoutException
@@ -8,14 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-from booking.helper import sleep_decorator, break_elements_in_selection_bar, validate_ages
-from booking.helper import check_date_format
-from booking.helper import is_valid_date
-from booking.helper import date_diff
+import booking.helper as bh
 import booking.constants as const
-from booking.constants import currencies
-from booking.constants import languages
 
 
 class Booking(webdriver.Chrome):
@@ -41,7 +35,7 @@ class Booking(webdriver.Chrome):
         self.maximize_window()
 
     def __exit__(self, exc_type, exc_val, exc_tb):  # exit function for context manager
-        if False:  # if exc_type is not None:  # if the program not closes properly -> close window
+        if exc_type is not None:  # if the program not closes properly -> close window
             self.quit()
 
     def land_first_page(self):
@@ -60,9 +54,9 @@ class Booking(webdriver.Chrome):
             # Handle the case where the element is not found within the timeout
             print("Popup button not found within the specified timeout.")
 
-    @sleep_decorator()
+    @bh.sleep_decorator()
     def change_lang(self, lang='US'):
-        if lang not in languages:
+        if lang not in const.languages:
             raise ValueError
 
         # Use a CSS_SELECTOR expression to find the language popup element
@@ -71,12 +65,12 @@ class Booking(webdriver.Chrome):
 
         # Use an XPath expression to find the button containing the English (US) text
         lang_button = self.find_element(By.XPATH,
-                                        f"//button[@data-testid='selection-item']//span[contains(text(),'{languages[lang]}')]")
+                                        f"//button[@data-testid='selection-item']//span[contains(text(),'{const.languages[lang]}')]")
         lang_button.click()
 
-    @sleep_decorator()
+    @bh.sleep_decorator()
     def change_currency(self, curr="USD"):
-        if curr not in currencies:
+        if curr not in const.currencies:
             raise ValueError
 
         # Use a CSS_SELECTOR expression to find the currency popup element
@@ -109,30 +103,98 @@ class Booking(webdriver.Chrome):
         )
         autocomplete_elm.click()
 
-    def choose_dates(self, checkin: str, checkout: str):
+    def flexible_vacation(self, duration: str, months: List[int]):
         """
-        The function get two string based dates,
+        duration: this parameter has four options, "weekend", "week", "month", "other-{num_days}-{from}",
+                   where num_days is in range [1,90] and from is weekday (Sunday, Monday, ect.) first letter capitalized
+        mounts: list of months that the vacation will take place at them, for use in sub function
+        """
+
+        if not (0 < len(months) <= 3) or duration.split('-')[0] not in const.flexible_duration_options:
+            raise ValueError
+
+        # move to flexible vacation mode
+        self.find_element(By.CSS_SELECTOR, "[aria-controls='flexible-searchboxdatepicker']").click()
+
+        # pick length of the vacation
+        self.__pick_duration_flexible(duration)
+
+        # pick the months that the vacation may take place at
+        try:
+            self.find_element(By.CSS_SELECTOR,
+                              "[data-testid='searchbox-dates-container'] button").click()  # reopen date menu
+        except:
+            pass
+        self.__pick_months(months)
+
+        # accept choices
+        self.find_element(By.XPATH, "//button[span[contains(., 'Select dates')]]").click()
+
+    def __pick_duration_flexible(self, duration: str):
+        if duration == "weekend":
+            self.find_element(By.XPATH, "//div[@data-testid='flexible-dates-day'][1]").click()
+        elif duration == "week":
+            self.find_element(By.XPATH, "//div[@data-testid='flexible-dates-day'][2]").click()
+        elif duration == "month":
+            self.find_element(By.XPATH, "//div[@data-testid='flexible-dates-day'][3]").click()
+        else:  # duration is a "other-{num_days}-{from}" string
+            self.find_element(By.XPATH, "//div[@data-testid='flexible-dates-day'][4]").click()
+            try:
+                _, num_days, weekday = duration.split('-')  # get necessary values
+                # select duration
+                num_days_elem = self.find_element(By.CSS_SELECTOR,
+                                                  "[data-testid='flexible-dates-days'] input[type='number']")
+                num_days_elem.click()  # focus on the element and keep the window from closing
+                num_days_elem.send_keys(num_days)
+                # select starting weekday
+                select = Select(self.find_element(By.CSS_SELECTOR, "[data-testid='flexible-dates-days'] select"))
+                select.select_by_visible_text(f"From {weekday}")
+            except:
+                raise ValueError
+
+    def __pick_months(self, months: List[int]):
+        """
+        the function get a list of months and picks them to be the mount of the vacation
+        """
+        if bh.validate_months(months) is False:
+            raise ValueError("Illegal month was picked")
+        next_months_btn = self.find_element(By.XPATH,
+                                            "//div[@data-testid='flexible-dates-months']//button[2]")
+        for month in sorted(months):
+            while True:
+                try:
+                    self.find_element(By.XPATH,
+                                      f"//span[contains(text(), '{const.MONTHS[month]}')]/ancestor::label[@data-testid='flexible-dates-month']//parent::*").click()
+                    break
+                except:
+                    for i in range(0, 20):  # click next months btn 20 times
+                        next_months_btn.click()
+
+    def choose_vacation_dates(self, checkin: str, checkout: str, flex: int = 0):
+        """
+        The function get two string based dates, and an integer
         at the first part, the function validate the dates in several checks,
-        and then it picks the date elements in the webdriver
+        and then it picks the date elements in the webdriver.
+        finally, the function calls to sub function that handle the flexibility part
         Note: dates are limited for 12 mounts from today to save time,
             to change that, please change the booking.constants.MAXMOUNTS
         """
         #  validate dates format
-        if check_date_format(checkin) is False or check_date_format(checkout) is False:
+        if bh.check_date_format(checkin) is False or bh.check_date_format(checkout) is False:
             raise ValueError
 
         #  validate dates as real dates
-        if is_valid_date(checkin) is False or is_valid_date(checkout) is False:
+        if bh.is_valid_date(checkin) is False or bh.is_valid_date(checkout) is False:
             raise ValueError
 
         #  checks that the checkin is today or after that, and that the checkout is after the checkin
-        if date_diff(None, checkin) < 0 or date_diff(checkin, checkout) < 0:
+        if bh.date_diff("", checkin) < 0 or bh.date_diff(checkin, checkout) < 0:
             raise ValueError
 
         max_tries = 0
         checked_in = False  # will help us to pick the checkin date at the beginning and the checkout after that
         next_date_btn = self.find_element(By.CSS_SELECTOR, "[id*='searchboxdatepicker'] button")  # next month btn
-        while max_tries < const.MAXMOUNTS:  # picked dates are in the following MAXMOUNTS (12) months:
+        while max_tries < const.MAX_MONTHS:  # picked dates are in the following MAXMOUNTS (12) months:
             try:
                 date = WebDriverWait(self, 0.5).until(
                     EC.element_to_be_clickable((By.CSS_SELECTOR,
@@ -146,10 +208,12 @@ class Booking(webdriver.Chrome):
             except Exception:
                 next_date_btn.click()  # if date not found in the presented calendar, change months
                 max_tries += 1
-                if max_tries == const.MAXMOUNTS:
+                if max_tries == const.MAX_MONTHS:
                     raise ValueError
 
-    def flexiable_dates(self, flex: int = 0):
+        self.__flexible_dates(flex)
+
+    def __flexible_dates(self, flex: int = 0):
         if flex not in const.flexible_dates_options.keys():
             return
         try:
@@ -157,9 +221,9 @@ class Booking(webdriver.Chrome):
                               f"//div[@data-testid='flexible-dates-container']//li[{const.flexible_dates_options[flex]}]") \
                 .click()
         except selenium.common.exceptions.NoSuchElementException:
-            # if button not found, that indicates that the date picking nav is closed and we need to reopen it.
+            # if button not found, that indicates that the date picking nav is closed, and we need to reopen it.
             self.find_element(By.XPATH, "//div[@data-testid='searchbox-dates-container']/button").click()
-            self.flexiable_dates(flex)
+            self.__flexible_dates(flex)
 
     def select_num_adults(self, num_of_adults: int = const.ADULTS_DEFAULT):
         if not (const.MIN_NUM_ADULTS <= num_of_adults <= const.MAX_NUM_ADULTS):
@@ -168,7 +232,7 @@ class Booking(webdriver.Chrome):
         selection_menu_btn.click()  # open selection menu
 
         # get necessary elements
-        minus_adult_btn, plus_adult_btn, curr_num_adults = break_elements_in_selection_bar(self, id="group_adults")
+        minus_adult_btn, plus_adult_btn, curr_num_adults = bh.break_elements_in_selection_bar(self, id="group_adults")
 
         if num_of_adults < curr_num_adults:
             for i in range(curr_num_adults, num_of_adults, -1):
@@ -185,7 +249,7 @@ class Booking(webdriver.Chrome):
         selection_menu_btn.click()  # open selection menu
 
         # get necessary elements
-        minus_room_btn, plus_room_btn, curr_num_rooms = break_elements_in_selection_bar(self, id="no_rooms")
+        minus_room_btn, plus_room_btn, curr_num_rooms = bh.break_elements_in_selection_bar(self, id="no_rooms")
 
         if num_of_rooms < curr_num_rooms:
             for i in range(curr_num_rooms, num_of_rooms, -1):
@@ -197,10 +261,10 @@ class Booking(webdriver.Chrome):
 
     def select_num_children(self, children_ages: List[int]):
 
-        if not (len(children_ages) <= const.MAX_NUM_CHILDREN) or validate_ages(children_ages) is False:
+        if not (len(children_ages) <= const.MAX_NUM_CHILDREN) or bh.validate_ages(children_ages) is False:
             raise ValueError
 
-        if len(children_ages) == const.CHILDREN_DEFULT:
+        if len(children_ages) == const.CHILDREN_DEFAULT:
             return
         selection_menu_btn = self.find_element(By.CSS_SELECTOR, "[data-testid='occupancy-config']")
         selection_menu_btn.click()  # open selection menu
@@ -209,16 +273,38 @@ class Booking(webdriver.Chrome):
         plus_child_btn = self.find_element(By.XPATH,
                                            "//input[@id='group_children']/parent::*//button[2]")
 
-        for i in range(const.CHILDREN_DEFULT, len(children_ages)):
+        for i in range(const.CHILDREN_DEFAULT, len(children_ages)):
             plus_child_btn.click()  # add child
 
-        for index, age in enumerate(children_ages, start=1):  # pick child age in dropdown manus
-            time.sleep(0.25)
+        for index, age in enumerate(children_ages, start=1):  # pick child age in dropdown menu
             select_elem = self.find_element(By.XPATH, f"//div[@data-testid='kids-ages-select'][{index}]//select")
+            select_elem.click()  # focus on the element and keep the window from closing
             select = Select(select_elem)
             select.select_by_value(age.__str__())
-            selection_menu_btn.click()  # reopen selection menu
+
         selection_menu_btn.click()  # close selection menu
+
+    def entire_place_option(self, check: bool = False):
+        """
+        This function will enable/disable the "I'm looking for an entire home or apartment" search option
+        """
+        option_elem = self.find_element(By.CSS_SELECTOR,
+                                        "[data-testid='searchbox-footer'] input[name='sb_entire_place']")
+
+        state = option_elem.is_selected()  # get checkbox state
+        if check != state:
+            option_elem.click()
+
+    def work_travel_option(self, check: bool = False):
+        """
+        This function will enable/disable the "I'm traveling for work" search option
+        """
+        option_elem = self.find_element(By.CSS_SELECTOR,
+                                        "[data-testid='searchbox-footer'] input[name='sb_travel_purpose']")
+
+        state = option_elem.is_selected()  # get checkbox state
+        if check != state:
+            option_elem.click()
 
     def submit_search(self):
         sub_btn = self.find_element(By.CSS_SELECTOR, "form[action*=searchresults] button[type='submit']")
